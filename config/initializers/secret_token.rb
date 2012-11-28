61eea63272c698c519bbb9cf29067c6fca7a4363
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-11-28 19:16:58 +0800

    Generate default rails app with using MySQL and without Test::Unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twtb::Application.config.secret_token = 'f363f0ad87eab69f7dc443631ade965066113ac68de097452ba05e5f28d40e2db3c770f328690af362f6239af77e00b0ca6066fcc75829b09301940c1a99f921'
